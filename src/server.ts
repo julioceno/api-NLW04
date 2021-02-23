5af66a880db14a67c49eeabf599f226775460281
Julio <julioreplay@gmail.com>
2021-02-23 10:17:18 -0300

    migrations done

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,29 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database"
 
 const app = express();
 
-app.listen(3333, () => console.log('Server is running!'))
+/*
+* GET => Buscar
+* POST => Salvar
+* PUT => Alterar
+* DELETE => Deletar
+* PATCH => Alteração específica 
+*/
+
+
+// https://localhost:3333/users
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello world - NLW04'})
+});
+
+// 1 param => Rota(Recurso API)
+// 2 param => request, response
+
+app.post('/', (request, response) => {
+    // recebeu os dados para salvar
+    return response.json({message: 'Os dados foram salvos com sucessos!'})
+});
+
+app.listen(3333, () => console.log('Server is running!'));
